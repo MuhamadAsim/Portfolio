cfbef4c9d2c4f0091375feaa4205f00de96d0bd7
Muhamad Asim <rao1235251325@gmail.com>
2026-09-04 15:16:17 +0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
- 
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          particles: ["@tsparticles/engine", "@tsparticles/react", "@tsparticles/slim"],
+          particles: [
+            "@tsparticles/engine",
+            "@tsparticles/react",
+            "@tsparticles/slim",
+          ],
           gsap: ["gsap"],
         },
       },
